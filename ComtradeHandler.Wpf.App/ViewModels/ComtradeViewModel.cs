70779d78f3e5f92928e6349f84f63724847f84dc
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2024-03-17 18:06:29 +0100

    Add ComtradeViewModel draft.

---
+++ ComtradeHandler.Wpf.App/ViewModels/ComtradeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ComtradeHandler.Core.Models;
+using Microsoft.Extensions.Logging;
+
+namespace ComtradeHandler.Wpf.App.ViewModels;
+public class ComtradeViewModel
+{
+    private readonly ILogger<ComtradeViewModel> _logger;
+
+    public ComtradeViewModel(ILogger<ComtradeViewModel> logger)
+    {
+        _logger = logger;
+    }
+
+    private ComtradeConfiguration? ComtradeConfiguration { get; set; }
+    private ComtradeData? ComtradeData { get; set; }
+
+    
+}
