eed4fbf5c64aafa49bb9a14b702aea93d99dcf98
gabrieldelaparra <ignacio.delaparra@gmail.com>
2026-02-06 10:19:59 +0100

    Update target framework to .NET 9.0 in project file.

--- ComtradeHandler.Wpf.App/ViewModels/ComtradeViewModel.cs
+++ ComtradeHandler.Wpf.App/ViewModels/ComtradeViewModel.cs
@@ -7,11 +7,75 @@ using ScottPlot;
 using ScottPlot.WPF;
 
 using System.Collections.ObjectModel;
+using System.Configuration;
+using ComtradeHandler.Core.Handlers;
+using System.Linq;
 
 namespace ComtradeHandler.Wpf.App.ViewModels;
+
+public class AnalogSignalGroup : ViewModelBase
+{
+    public AnalogSignalGroup(ObservableCollection<AnalogSignal> analogSignals)
+    {
+        AnalogSignals = analogSignals;
+    }
+
+    public ObservableCollection<AnalogSignal> AnalogSignals { get; }
+    public WpfPlot WpfPlot
+    {
+        get {
+            var signals = AnalogSignals.Select(x => x.Signal).ToList();
+
+            var wpfPlot = new WpfPlot();
+            foreach (var signal in signals)
+                wpfPlot.Plot.Add.Signal(signal);
+
+            wpfPlot.Plot.Layout.Fixed(new PixelPadding(60, 10, 30, 10));
+
+            var min = signals.SelectMany(x => x).Min();
+            var max = signals.SelectMany(x => x).Max();
+            var hGap = 20;
+            var d = 0.25;
+
+            var minX = 0 - hGap;
+            var maxX = signals.First().Length + hGap;
+            var gMin = Math.Max(Math.Abs(min * d), hGap);
+            var gMax = Math.Max(Math.Abs(max * d), hGap);
+            var minY = min - gMin;
+            var maxY = max + gMax;
+
+            ScottPlot.AxisRules.MaximumBoundary maximumBoundary = new(
+                xAxis: wpfPlot.Plot.Axes.Bottom,
+                yAxis: wpfPlot.Plot.Axes.Left,
+                limits: new AxisLimits(minX, maxX, minY, maxY));
+
+            ScottPlot.AxisRules.LockedVertical lockedVertical = new(wpfPlot.Plot.Axes.Left);
+
+            wpfPlot.Plot.Axes.Rules.Clear();
+            wpfPlot.Plot.Axes.Rules.Add(maximumBoundary);
+            wpfPlot.Plot.Axes.Rules.Add(lockedVertical);
+
+            wpfPlot.Refresh();
+
+            return wpfPlot;
+        }
+    }
+
+}
+public class AnalogSignal : ViewModelBase
+{
+    public AnalogSignal(double[] signal)
+    {
+        Signal = signal;
+    }
+
+    public double[] Signal { get; }
+}
+
 public class ComtradeViewModel : ViewModelBase
 {
     private readonly ILogger<ComtradeViewModel> _logger;
+    public ObservableCollection<AnalogSignalGroup> AnalogSignalGroups { get; set; }
 
     public ComtradeViewModel(ILogger<ComtradeViewModel> logger)
     {
@@ -20,10 +84,19 @@ public class ComtradeViewModel : ViewModelBase
         var cfgFile = "Resources\\Comtrade1.cfg";
         var datFile = "Resources\\Comtrade1.dat";
 
+        AnalogSignalGroups = new ObservableCollection<AnalogSignalGroup>();
+
         WpfPlots = new ObservableCollection<WpfPlot>();
 
-        var reader = new ComtradeHandler.Core.Handlers.RecordReader(datFile);
-        ComtradeConfiguration = reader.Configuration;
+        var reader = new RecordReader(datFile);
+
+        var signals = Enumerable.Range(0, reader.Configuration.AnalogChannelsCount).Select(x => new AnalogSignal(reader.GetAnalogPrimaryChannel(x).ToArray()));
+
+
+        var ag1 = new AnalogSignalGroup(signals.Skip())
+
+
+        ComtradeConfiguration = reader.Configuration};
         ComtradeData = reader.Data;
 
         for (int i = 0; i < reader.Configuration.AnalogChannelsCount; i++) {
@@ -73,7 +146,6 @@ public class ComtradeViewModel : ViewModelBase
         foreach (var dest in WpfPlots) {
             if (dest == sender)
                 continue;
-
 
             AxisLimits axesBefore = dest.Plot.Axes.GetLimits();
             dest.Plot.Axes.SetLimitsX(source.Axes.GetLimits());
